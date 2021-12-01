3e88e7e70c277b5a25d303fbbef117ea6ef41f47
Jonathan Reyes <j@jreyes.org>
2021-12-01 15:58:15 -0500

    Solve 2021 day 1

---
+++ 2021/README.md
@@ -0,0 +1,9 @@
+# 2021
+
+My solutions to [Advent of Code 2021](https://adventofcode.com/2021)
+
+## Running Zig Solutions
+
+```
+zig run zig/<DAY>.zig
+```
---
+++ 2021/zig/01.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    var gpalloc = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(!gpalloc.deinit());
+    const file = try std.fs.cwd().openFile("../inputs/01.txt", .{});
+    defer file.close();
+    const reader = std.io.bufferedReader(file.reader()).reader();
+    var buf: [8]u8 = undefined;
+    var numbers = std.ArrayList(i32).init(&gpalloc.allocator);
+    defer numbers.deinit();
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const n = try std.fmt.parseInt(i32, line, 10);
+        try numbers.append(n);
+    }
+    var sums = std.ArrayList(i32).init(&gpalloc.allocator);
+    defer sums.deinit();
+    try sums.ensureTotalCapacity(numbers.items.len - 2);
+    var i: usize = 0;
+    while (i < numbers.items.len - 2) : (i += 1) {
+        try sums.append(numbers.items[i] + numbers.items[i + 1] + numbers.items[i + 2]);
+    }
+    // Also possible:
+    // for (numbers.items[2..]) |n, i| {
+    //   try sums.append(n + numbers.items[i] + numbers.items[i + 1]);
+    // }
+    std.debug.print("part 1: {d}\n", .{count_increased(numbers.items)});
+    std.debug.print("part 2: {d}\n", .{count_increased(sums.items)});
+}
+
+fn count_increased(numbers: []const i32) u32 {
+    var count_inc: u32 = 0;
+    var prev = numbers[0];
+    for (numbers[1..]) |n| {
+        if (n > prev) {
+            count_inc += 1;
+        }
+        prev = n;
+    }
+    return count_inc;
+}
